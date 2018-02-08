51fcb587abfc6f03da85a14273332881b021c575
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2018-02-08 09:33:28 -0200

    Pequenos ajustes
    Pequenos ajustes visuais no código

--- application.js
+++ application.js
@@ -1,67 +1,67 @@
 var app = angular.module("aplicativo", []);
-    app.controller("usercontroller", function($scope, $http){
-        $scope.acaoBotao = "Cadastrar";
-        $scope.inserirDados = function(){
-            if($scope.nome == null)
-            {
-                alert("O campo nome não pode estar vazio");
-            }
-            else if($scope.sobrenome == null)
-            {
-                alert("O campo sobrenome não pode estar vazio");
-            }
-            else
-            {
-                $http.post("insert.php", { 'nome' : $scope.nome, 'sobrenome' : $scope.sobrenome, 'acaoBotao' : $scope.acaoBotao, 'id' : $scope.id } ).
-                then(function (success){
-                    if($scope.acaoBotao == "Cadastrar")
-                    {
-                        alert('Dados enviados com sucesso! Nome: ' + $scope.nome + '. Sobrenome: ' + $scope.sobrenome);
-                    }
-                    else if($scope.acaoBotao == "Atualizar")
-                    {
-                        alert('Dados atualizados com sucesso! Nome: ' + $scope.nome + '. Sobrenome: ' + $scope.sobrenome);
-                    }                    
-                    $scope.nome = null;
-                    $scope.sobrenome = null;
-                    $scope.acaoBotao = "Cadastrar";
-                    $scope.mostrarDados();
-                },
-                function (error){
-                    alert('Falha ao enviar o formulário!');
-                });
-            }
+app.controller("usercontroller", function($scope, $http){
+    $scope.acaoBotao = "Cadastrar";
+    $scope.inserirDados = function(){
+        if($scope.nome == null)
+        {
+            alert("O campo nome não pode estar vazio");
         }
-        $scope.mostrarDados = function(){
-            $http.get("select.php").
+        else if($scope.sobrenome == null)
+        {
+            alert("O campo sobrenome não pode estar vazio");
+        }
+        else
+        {
+            $http.post("insert.php", { 'nome' : $scope.nome, 'sobrenome' : $scope.sobrenome, 'acaoBotao' : $scope.acaoBotao, 'id' : $scope.id } ).
             then(function (success){
-                $scope.nomes = success.data;
+                if($scope.acaoBotao == "Cadastrar")
+                {
+                    alert('Dados enviados com sucesso! Nome: ' + $scope.nome + '. Sobrenome: ' + $scope.sobrenome);
+                }
+                else if($scope.acaoBotao == "Atualizar")
+                {
+                    alert('Dados atualizados com sucesso! Nome: ' + $scope.nome + '. Sobrenome: ' + $scope.sobrenome);
+                }                    
+                $scope.nome = null;
+                $scope.sobrenome = null;
+                $scope.acaoBotao = "Cadastrar";
+                $scope.mostrarDados();
             },
-            function failed(error){
-                $scope.nomes = "Falha ao carregar";
+            function (error){
+                alert('Falha ao enviar o formulário!');
             });
         }
-        $scope.atualizarDados = function(id, nome, sobrenome){
-            $scope.id = id;
-            $scope.nome = nome;
-            $scope.sobrenome = sobrenome;
-            $scope.acaoBotao = "Atualizar";
+    }
+    $scope.mostrarDados = function(){
+        $http.get("select.php").
+        then(function (success){
+            $scope.nomes = success.data;
+        },
+        function failed(error){
+            $scope.nomes = "Falha ao carregar";
+        });
+    }
+    $scope.atualizarDados = function(id, nome, sobrenome){
+        $scope.id = id;
+        $scope.nome = nome;
+        $scope.sobrenome = sobrenome;
+        $scope.acaoBotao = "Atualizar";
+    }
+    $scope.apagarDados = function(id){
+        if(confirm("Têm certeza que quer apagar este usuário?"))
+        {
+            $http.post("delete.php", {'id' : id}).
+            then(function (success){
+                alert("Registro apagado com sucesso!");
+                $scope.mostrarDados();
+            },
+            function failed(error){
+                alert("Falha ao apagar o registro desejado!");
+            });
         }
-        $scope.apagarDados = function(id){
-            if(confirm("Têm certeza que quer apagar este usuário?"))
-            {
-                $http.post("delete.php", {'id' : id}).
-                then(function (success){
-                    alert("Registro apagado com sucesso!");
-                    $scope.mostrarDados();
-                },
-                function failed(error){
-                    alert("Falha ao apagar o registro desejado!");
-                });
-            }
-            else
-            {
-                return false;
-            }
+        else
+        {
+            return false;
         }
-    });
+    }
+});
