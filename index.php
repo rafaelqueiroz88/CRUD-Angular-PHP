51d9e9cf1e1bec1705dfb8d8cb8a861a5b947ac9
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2018-02-08 09:55:09 -0200

    Visual responsivo
    Implementação do visual responsivo.

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 
         <div class="container" ng-app="aplicativo" ng-controller="usercontroller" ng-init="mostrarDados()">
             <div class="row">
-                <div class="col col-3"></div>
-                <div class="col col-6">
+                <div class="col-xs-0 col-sm-0 col-md-3 col-lg-3 col-xl-3"></div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
 
                     <h1 align="center">Cadastro</h1>
                     <hr />
@@ -30,8 +30,8 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col col-3"></div>
-                <div class="col col-6">
+                <div class="col-xs-0 col-sm-0 col-md-3 col-lg-3 col-xl-3"></div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
 
                     <label for="nome">Nome: </label>
                     <input type="text" class="form-control" name="_nome" ng-model="nome" />
@@ -39,8 +39,8 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col col-3"></div>
-                <div class="col col-6">
+                <div class="col-xs-0 col-sm-0 col-md-3 col-lg-3 col-xl-3"></div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
 
                     <label for="sobrenome">Sobrenome: </label>
                     <input type="text" class="form-control" name="_sobrenome" ng-model="sobrenome" />
@@ -48,8 +48,8 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col col-3"></div>
-                <div class="col col-6">
+                <div class="col-xs-0 col-sm-0 col-md-3 col-lg-3 col-xl-3"></div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
 
                     <br />
                     <button class="btn btn-outline-primary btn-sm btn-block" ng-click="inserirDados()">
@@ -59,36 +59,41 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col col-3"></div>
-                <div class="col col-6">
+                <div class="col-xs-0 col-sm-0 col-md-3 col-lg-3 col-xl-3"></div>
+                <div class="col-xs-10 col-sm-10 col-md-6 col-lg-6 col-xl-6">
 
                     <hr />
-                    <table class="table table-bordered">
-                        <thead>
-                            <th>Nome</th>
-                            <th>Sobrenome</th>
-                            <th style="text-align: center;">Ações</th>
-                        </thead>
-                        <tbody>
-                            <tr ng-repeat="x in nomes">
-                                <td>
-                                    {{x.nome}}
-                                </td>
-                                <td>
-                                    {{x.sobrenome}}
-                                </td>
-                                <td align="center">
-                                    <button class="btn btn-outline-info" ng-click="atualizarDados(x.id, x.nome, x.sobrenome)">
-                                        Atualizar
-                                    </button>
-                                    
-                                    <button class="btn btn-danger" ng-click="apagarDados(x.id)">
-                                        Apagar
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover">
+                            <thead>
+                                <tr class="table-default">
+                                    <th>Nome</th>
+                                    <th>Sobrenome</th>
+                                    <th style="text-align: center;">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr ng-repeat="x in nomes">
+                                    <td>
+                                        {{x.nome}}
+                                    </td>
+                                    <td>
+                                        {{x.sobrenome}}
+                                    </td>
+                                    <td align="center">
+                                        <button class="btn btn-outline-info btn-sm" ng-click="atualizarDados(x.id, x.nome, x.sobrenome)">
+                                            Atualizar
+                                        </button>
+                                        
+                                        <button class="btn btn-danger btn-sm" ng-click="apagarDados(x.id)">
+                                            Apagar
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>                    
 
                 </div>
             </div>
